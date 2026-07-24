94cad444b0b3a536e07835ea8155b9508a76b402
not_luke <not_luke@fake.com>
2026-07-24 14:36:02 +1000

    wip

--- evil_usb/usb.gd
+++ evil_usb/usb.gd
@@ -1,43 +1,98 @@
 extends RigidBody2D
-var solved: bool = false
+
+enum UsbStatus{
+		FREE,
+		ENTERING,
+		WON
+}
+
+enum UsbOrient{
+		A,
+		B,
+		C,
+		D,
+}
+
 @onready
 var usb_sprite: AnimatedSprite2D = %usb_sprite
 
 @onready
+var slot_mask: Sprite2D = %computah_mask
+
+@onready
 var animation_player: AnimationPlayer= %AnimationPlayer
+
+@onready
+var status: UsbStatus = UsbStatus.FREE
+
+@onready
+var orient: UsbOrient = UsbOrient.A
+
+const win_orient: UsbOrient = UsbOrient.C
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if !solved:
-		if usb_sprite.frame == 3:
-			self.collision_mask = 0b00000000_00000000_00000000_00000010
-		else:
-			self.collision_mask = 0b00000000_00000000_00000000_00000001
-		var vector: Vector2 = Vector2(get_viewport().get_mouse_position()- position)
-		move_and_collide(vector)
-		# position = position.lerp(get_viewport().get_mouse_position(), 2*delta)
-		if Input.is_action_just_pressed("left_click"):
-			usb_flip()
+	check_won()
+	update_collisions()
+	move_usb()
+	if Input.is_action_just_pressed("left_click"):
+		usb_flip()
+	render_usb()
+
+func check_won() -> void:
+	if status == UsbStatus.WON:
+		print("won!")
+
+func update_collisions() -> void:
+	if orient != win_orient:
+		self.collision_mask = 0b00000000_00000000_00000000_00000001
+	else:
+		self.collision_mask = 0b00000000_00000000_00000000_00000010
+
+func move_usb() -> void:
+	var vector: Vector2 = Vector2(get_viewport().get_mouse_position()- position)
+	move_and_collide(vector)
 
 func usb_flip() -> void:
-	if usb_sprite.frame == 3:
-		usb_sprite.frame = 0
-	else:
-		usb_sprite.frame += 1
+	match orient:
+		UsbOrient.A:
+			orient = UsbOrient.B
+		UsbOrient.B:
+			orient = UsbOrient.C
+		UsbOrient.C:
+			orient = UsbOrient.D
+		UsbOrient.D:
+			orient = UsbOrient.A
 
-func _on_slot_area_entered(area: Area2D, source: Area2D) -> void:
-	if usb_sprite.frame == 3:
-		solved = true
+func render_usb() -> void:
+	match orient:
+		UsbOrient.A:
+			usb_sprite.frame = 0
+		UsbOrient.B:
+			usb_sprite.frame = 1
+		UsbOrient.C:
+			usb_sprite.frame = 2
+		UsbOrient.D:
+			usb_sprite.frame = 3
 
-		animation_player.play("w2")
+# When  hitting the win collision
+func _on_slot_body_entered(_body: Node2D) -> void:
+	if (status == UsbStatus.ENTERING && orient == win_orient):
+		status = UsbStatus.WON
+		# animation_player.play("w2")
 		print("correct")
 
-func _on_slot_body_entered(body: Node2D) -> void:
-	if usb_sprite.frame == 3:
-		solved = true
+# When entering
+func _on_slot_entered_collision_body_entered(_body: Node2D) -> void:
+	if orient == win_orient:
+		slot_mask.z_index = 2
+		status = UsbStatus.ENTERING
 
-		animation_player.play("w2")
-		print("correct")
+# When exiting
+func _on_slot_entered_collision_body_exited(_body: Node2D) -> void:
+	slot_mask.z_index = -1
+	status = UsbStatus.FREE
