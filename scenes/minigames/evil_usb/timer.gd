af909940233e906ad2a5f663077ba7a1d4546943
not_luke <not_luke@fake.com>
2026-07-24 21:08:47 +1000

    add timer to usb game

--- scenes/minigames/evil_usb/timer.gd
+++ scenes/minigames/evil_usb/timer.gd
@@ -1,12 +1,19 @@
 extends Timer
 
+var lost: bool = false
+
 func _ready() -> void:
 	self.timeout.connect(_handle_timeout)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if lost && Input.is_action_just_pressed("left_click"):
+		get_tree().paused = false
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _handle_timeout() -> void:
+	lost = true
+	self.paused = true
 	var lose_banner: Label = self.get_child(0)
 	lose_banner.visible = true
+	get_tree().paused = true
