a228ce3327e39233c17f9f57b31877f69dab4917
DigitalData <legoxavierlocketravers@gmail.com>
2026-07-24 21:18:47 +1000

    feet: Hanging Finger sfx

--- scenes/minigames/finger_hammer/hanging_finger.gd
+++ scenes/minigames/finger_hammer/hanging_finger.gd
@@ -1,37 +1,50 @@
-extends Area2D
+extends Node2D
 
 class_name HangingFinger2D
 
 @export var smash_timeout: float = 1
 var is_smashed: bool = false
 var is_hovered: bool = false
+var finger_animations: AnimationPlayer 
+var smashed_timer: Node2D
+
+func _ready() -> void:
+    finger_animations = %FingerAnimations
+    smashed_timer = %SmashedTimer
 
 signal smashed(finger: HangingFinger2D)
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		if is_hovered:
-			smash()
+    if event is InputEventMouseButton and event.is_pressed():
+        if is_hovered:
+            smash()
 
 func smash() -> void:
-	%FingerAnimations.play("hit")
-	%SmashedTimer.start(smash_timeout)
-	is_smashed = true
-	is_hovered = false
-	emit_signal("smashed", self)
+    finger_animations.play("hit")
+    smashed_timer.show()
+    (smashed_timer.get_node("Timer") as Timer).start(smash_timeout)
+    is_smashed = true
+    is_hovered = false
+    emit_signal("smashed", self)
+    (%YelpSFX as AudioStreamPlayer).play()
 
 func recover() -> void:
-	%SmashedTimer.stop()
-	is_smashed = false
-	%FingerAnimations.play_backwards("hit")
-	
+    smashed_timer.hide()    
+    (smashed_timer.get_node("Timer") as Timer).stop()
+    is_smashed = false
+    finger_animations.play_backwards("hit")
+    
 
 func start_hover() -> void:
-	if not is_smashed:
-		is_hovered = true
-		%FingerAnimations.play("shaking")
+    if not is_smashed:
+        is_hovered = true
+        finger_animations.play("shaking")
 
 func end_hover() -> void:
-	if not is_smashed:
-		is_hovered = false
-		%FingerAnimations.stop()
+    if not is_smashed:
+        is_hovered = false
+        finger_animations.stop()
+
+func hand_smashed() -> void:
+    smashed_timer.hide()    
+    (smashed_timer.get_node("Timer") as Timer).stop()
