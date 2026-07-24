b9e4411057b5b897e0495ac8577f9145d1d15660
Tom Frantz <thomas.albert.frantz@gmail.com>
2026-07-24 15:52:10 +1000

    feat: more fade stuff idfk?

--- scenes/common/hand_2.gd
+++ scenes/common/hand_2.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
+@export var FOLLOW_SPEED = 4.0
+
 var current_collisions
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
-	%Node2D.position = mouse_pos
+	%CharacterBody2D.position  = %CharacterBody2D.position.lerp(mouse_pos, delta * FOLLOW_SPEED)
+
 	
 func _input(event: InputEvent) -> void:
 	var ga: Area2D = %GrabArea
