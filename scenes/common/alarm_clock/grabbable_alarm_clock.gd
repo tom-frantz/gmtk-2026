7f40a14a77a82e568c8d86e267b053d0c83d3afa
Thomas Frantz <thomas.albert.frantz@gmail.com>
2026-07-25 12:52:59 +1000

    fix: only emit signal if actually 'clickable' for game end

--- scenes/common/alarm_clock/grabbable_alarm_clock.gd
+++ scenes/common/alarm_clock/grabbable_alarm_clock.gd
@@ -1,8 +1,8 @@
 extends Grabbable
 
 
-
 func _on_alarm_button_body_entered(body: Node2D) -> void:
-	emit_signal("my_fuckin_ace_signal", body)
+	if collision_layer == 1:
+		emit_signal("my_fuckin_ace_signal", body)
 
 signal my_fuckin_ace_signal(body: Node2D)
