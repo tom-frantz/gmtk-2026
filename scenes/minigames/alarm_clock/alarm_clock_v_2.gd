193dfb1261beceb43ff1a2a552dda0a67e1896cc
not_luke <not_luke@fake.com>
2026-07-25 17:02:39 +1000

    tabs to spaces, turn off enforced types (thanks tom >:) )

--- scenes/minigames/alarm_clock/alarm_clock_v_2.gd
+++ scenes/minigames/alarm_clock/alarm_clock_v_2.gd
@@ -6,26 +6,26 @@ var has_won: bool = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY, 981) # Sets new gravity magnitude
-	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2(0, 1)) # Sets downward gravity
-	%background_music.play()
-	timer = get_node("/root/AlarmClockV2/LevelTimer/Timer")
-	background_music = %background_music
+    PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY, 981) # Sets new gravity magnitude
+    PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2(0, 1)) # Sets downward gravity
+    %background_music.play()
+    timer = get_node("/root/AlarmClockV2/LevelTimer/Timer")
+    background_music = %background_music
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if not background_music.playing and not timer.paused:
-		background_music.play()
+    if not background_music.playing and not timer.paused:
+        background_music.play()
 
 
 func _on_alarm_clock_my_fuckin_ace_signal(body: Node2D) -> void:
-	# Winner winner, chicken dinner.
-	var win_banner: Label = %win_banner
-	win_banner.visible = true
-	timer.paused = true
-	background_music.stop()
-	if !%win_audio.playing:
-		%win_audio.play()
-	await get_tree().create_timer(2.0).timeout
-	get_tree().change_scene_to_file("res://scenes/minigames/evil_usb/minigame_usb.tscn")
+    # Winner winner, chicken dinner.
+    var win_banner: Label = %win_banner
+    win_banner.visible = true
+    timer.paused = true
+    background_music.stop()
+    if !%win_audio.playing:
+        %win_audio.play()
+    await get_tree().create_timer(2.0).timeout
+    get_tree().change_scene_to_file("res://scenes/minigames/evil_usb/minigame_usb.tscn")
--- scenes/minigames/evil_usb/usb.gd
+++ scenes/minigames/evil_usb/usb.gd
@@ -1,17 +1,17 @@
 extends RigidBody2D
 
 enum UsbStatus{
-		FREE,
-		REJECT,
-		ENTERING,
-		WON
+        FREE,
+        REJECT,
+        ENTERING,
+        WON
 }
 
 enum UsbOrient{
-		A,
-		B,
-		C,
-		D,
+        A,
+        B,
+        C,
+        D,
 }
 
 @onready
@@ -39,110 +39,110 @@ const win_orient: UsbOrient = UsbOrient.C
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+    pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if !tween:
-		self.rotation_degrees = 0;
-		usb_sprite.rotation_degrees = 0;
+    if !tween:
+        self.rotation_degrees = 0;
+        usb_sprite.rotation_degrees = 0;
 
-	check_won()
-	update_collisions()
-	move_usb()
-	if Input.is_action_just_pressed("left_click"):
-		usb_flip()
-	render_usb()
+    check_won()
+    update_collisions()
+    move_usb()
+    if Input.is_action_just_pressed("left_click"):
+        usb_flip()
+    render_usb()
 
 func check_won() -> void:
-	if status == UsbStatus.WON:
-		var win_banner: Label = %win_banner
-		win_banner.visible = true
-		var timer: Timer = get_node("/root/MinigameUsb/LevelTimer/Timer")
-		timer.paused = true
-		var background_music: AudioStreamPlayer = %background_music
-		background_music.stop()
-		if !win_audio.playing:
-			win_audio.play()
-		await get_tree().create_timer(2.0).timeout
-		get_tree().change_scene_to_file("res://scenes/minigames/finger_hammer/minigame_finger_hammer.tscn")
+    if status == UsbStatus.WON:
+        var win_banner: Label = %win_banner
+        win_banner.visible = true
+        var timer: Timer = get_node("/root/MinigameUsb/LevelTimer/Timer")
+        timer.paused = true
+        var background_music: AudioStreamPlayer = %background_music
+        background_music.stop()
+        if !win_audio.playing:
+            win_audio.play()
+        await get_tree().create_timer(2.0).timeout
+        get_tree().change_scene_to_file("res://scenes/minigames/finger_hammer/minigame_finger_hammer.tscn")
 
 
 func update_collisions() -> void:
-	var free: int = 0b00000000_00000000_00000000_00000010
-	var blocked: int = 0b00000000_00000000_00000000_00000001
-	var entering: int = 0b00000000_00000000_00000000_00000100
-	self.collision_mask = blocked
-	if orient == win_orient:
-		self.collision_mask = free
-		if status == UsbStatus.ENTERING:
-			self.collision_mask |= entering
+    var free: int = 0b00000000_00000000_00000000_00000010
+    var blocked: int = 0b00000000_00000000_00000000_00000001
+    var entering: int = 0b00000000_00000000_00000000_00000100
+    self.collision_mask = blocked
+    if orient == win_orient:
+        self.collision_mask = free
+        if status == UsbStatus.ENTERING:
+            self.collision_mask |= entering
 
 func move_usb() -> void:
-	if status != UsbStatus.WON:
-		var target_pos: Vector2 = lerp(self.global_position, get_viewport().get_mouse_position(), 0.5)
-		var displacement: Vector2 = target_pos - self.global_position
-		var collision: KinematicCollision2D  = move_and_collide(displacement)
+    if status != UsbStatus.WON:
+        var target_pos: Vector2 = lerp(self.global_position, get_viewport().get_mouse_position(), 0.5)
+        var displacement: Vector2 = target_pos - self.global_position
+        var collision: KinematicCollision2D  = move_and_collide(displacement)
 
-		if status != UsbStatus.REJECT:
-			if collision && !tween:
-				var obj: Node2D = collision.get_collider()
-				if obj.name == "slot_body":
-					bounce_away(collision)
+        if status != UsbStatus.REJECT:
+            if collision && !tween:
+                var obj: Node2D = collision.get_collider()
+                if obj.name == "slot_body":
+                    bounce_away(collision)
 
 func bounce_away(collision: KinematicCollision2D) -> void:
-	status = UsbStatus.REJECT
-	tween = self.create_tween()
-	incorrect_audio.play()
-	tween.tween_property(self, "position", self.position + 30*collision.get_normal() , 0.1)
-	tween.tween_property(self, "rotation_degrees", self.rotation_degrees + 5, 0.1)
-	tween.tween_property(self, "rotation_degrees", self.rotation_degrees - 5, 0.1)
-	tween.tween_property(self, "rotation_degrees", self.rotation_degrees, 0.1)
-	tween.set_trans(Tween.TRANS_SINE)
-	tween.set_ease(Tween.EASE_IN_OUT)
-	tween.play()
-	tween.finished.connect(_on_animation_completed)
+    status = UsbStatus.REJECT
+    tween = self.create_tween()
+    incorrect_audio.play()
+    tween.tween_property(self, "position", self.position + 30*collision.get_normal() , 0.1)
+    tween.tween_property(self, "rotation_degrees", self.rotation_degrees + 5, 0.1)
+    tween.tween_property(self, "rotation_degrees", self.rotation_degrees - 5, 0.1)
+    tween.tween_property(self, "rotation_degrees", self.rotation_degrees, 0.1)
+    tween.set_trans(Tween.TRANS_SINE)
+    tween.set_ease(Tween.EASE_IN_OUT)
+    tween.play()
+    tween.finished.connect(_on_animation_completed)
 
 func _on_animation_completed() -> void:
-	tween.stop()
-	tween = null
-	status = UsbStatus.FREE
+    tween.stop()
+    tween = null
+    status = UsbStatus.FREE
 
 func usb_flip() -> void:
-	if status == UsbStatus.FREE:
-		match orient:
-			UsbOrient.A:
-				orient = UsbOrient.B
-			UsbOrient.B:
-				orient = UsbOrient.C
-			UsbOrient.C:
-				orient = UsbOrient.D
-			UsbOrient.D:
-				orient = UsbOrient.A
+    if status == UsbStatus.FREE:
+        match orient:
+            UsbOrient.A:
+                orient = UsbOrient.B
+            UsbOrient.B:
+                orient = UsbOrient.C
+            UsbOrient.C:
+                orient = UsbOrient.D
+            UsbOrient.D:
+                orient = UsbOrient.A
 
 func render_usb() -> void:
-	match orient:
-		UsbOrient.A:
-			usb_sprite.frame = 0
-		UsbOrient.B:
-			usb_sprite.frame = 1
-		UsbOrient.C:
-			usb_sprite.frame = 2
-		UsbOrient.D:
-			usb_sprite.frame = 3
+    match orient:
+        UsbOrient.A:
+            usb_sprite.frame = 0
+        UsbOrient.B:
+            usb_sprite.frame = 1
+        UsbOrient.C:
+            usb_sprite.frame = 2
+        UsbOrient.D:
+            usb_sprite.frame = 3
 
 # When  hitting the win collision
 func _on_slot_body_entered(_body: Node2D) -> void:
-	if (status == UsbStatus.ENTERING && orient == win_orient):
-		status = UsbStatus.WON
+    if (status == UsbStatus.ENTERING && orient == win_orient):
+        status = UsbStatus.WON
 
 # When entering
 func _on_slot_entered_collision_body_entered(_body: Node2D) -> void:
-	if orient == win_orient:
-		slot_mask.z_index = 2
-		status = UsbStatus.ENTERING
+    if orient == win_orient:
+        slot_mask.z_index = 2
+        status = UsbStatus.ENTERING
 
 # When exiting
 func _on_slot_entered_collision_body_exited(_body: Node2D) -> void:
-	slot_mask.z_index = -1
-	status = UsbStatus.FREE
+    slot_mask.z_index = -1
+    status = UsbStatus.FREE
