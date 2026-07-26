758a0175835c72c5cc55da61278f2301e8117362
Tom Frantz <thomas.albert.frantz@gmail.com>
2026-07-26 17:07:27 +1000

    chore: formatting

--- scenes/common/alarm_clock/grabbable_alarm_clock.gd
+++ scenes/common/alarm_clock/grabbable_alarm_clock.gd
@@ -8,10 +8,9 @@ func _on_alarm_button_body_entered(body: Node2D) -> void:
 signal my_fuckin_ace_signal(body: Node2D)
 
 func _ready() -> void:
-    was_pressed = false
+	was_pressed = false
 
 func _on_alarm_button_body_entered(body: Node2D) -> void:
-    if collision_layer == 1 and not was_pressed:
-        was_pressed = true
-        emit_signal("my_fuckin_ace_signal", body)
-
+	if collision_layer == 1 and not was_pressed:
+		was_pressed = true
+		emit_signal("my_fuckin_ace_signal", body)
