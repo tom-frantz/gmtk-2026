bf40aed4584b60444393d98137d702d652718719
not_luke <not_luke@fake.com>
2026-07-26 16:52:13 +1000

    end of world animated

--- scenes/the_end/its_over.gd
+++ scenes/the_end/its_over.gd
@@ -4,8 +4,13 @@ extends AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     self.play("boom")
+    animation_finished.connect(_animation_finished)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     pass
+    
+func _animation_finished() -> void:
+    var the_end_label: Label = %game_over_label
+    the_end_label.visible = true
