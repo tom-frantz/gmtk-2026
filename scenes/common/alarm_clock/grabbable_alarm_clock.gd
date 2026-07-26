cf35059d32d8cddc0618ac458289715931f627c4
Tom Frantz <thomas.albert.frantz@gmail.com>
2026-07-26 17:13:07 +1000

    fix: merge conflicts

--- scenes/common/alarm_clock/grabbable_alarm_clock.gd
+++ scenes/common/alarm_clock/grabbable_alarm_clock.gd
@@ -1,11 +1,8 @@
 extends Grabbable
 
-
-func _on_alarm_button_body_entered(body: Node2D) -> void:
-	if collision_layer == 1:
-		emit_signal("my_fuckin_ace_signal", body)
-
 signal my_fuckin_ace_signal(body: Node2D)
+
+var was_pressed: bool 
 
 func _ready() -> void:
 	was_pressed = false
