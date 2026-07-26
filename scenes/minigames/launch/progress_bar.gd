fc0015e7b9234ed2b9d6aeea0fb89dd9b7f835ce
not_luke <not_luke@fake.com>
2026-07-26 16:10:03 +1000

    added postit note and music to launch minigame

--- scenes/minigames/launch/progress_bar.gd
+++ scenes/minigames/launch/progress_bar.gd
@@ -1,7 +1,6 @@
 extends ProgressBar
 
-@onready
-var win_audio: AudioStreamPlayer = $"../win_audio"
+signal win
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,17 +14,7 @@ func _process(_delta: float) -> void:
     if value < 100:
         self.value -= 0.3
     else:
-        win()
+        emit_signal("win")
 
 func _on_texture_button_pressed() -> void:
     value +=6
-
-func win() -> void:
-    var win_banner: Label = %win_banner
-    win_banner.visible = true
-    var timer: Timer = $"../LevelTimer/Timer"
-    timer.paused = true
-    if !win_audio.playing:
-        win_audio.play()
-    await get_tree().create_timer(2.0).timeout
-    get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
