9f5f45d6fc5d458316c2002ab465b67f99a35c98
DigitalData <legoxavierlocketravers@gmail.com>
2026-07-26 14:14:12 +1000

    Create Level Timer Scene

---
+++ scenes/common/timer/level_timer.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+class_name LevelTimer
+
+@export_range(0, 120, 0.1, "suffix:s", "prefer_slider") var lose_timeout: float
+@export_range(0, 90, 1, "degrees", "prefer_slider") var transition_hand_angle: float
+@export var transition_scene: PackedScene
+@export var transition_text: String
+@export var next_minigame: PackedScene
+@export var lose_scene: PackedScene
+@export var background_music: AudioStream
+var background_music_player: AudioStreamPlayer
+var lose_timer: DigitalTimer2D
+var lose_screen: Node2D
+var lose_audio: AudioStreamPlayer
+var win_screen: Node2D
+var win_audio: AudioStreamPlayer
+
+func _ready() -> void:
+    background_music_player = %BackgroundMusic
+    lose_timer = %LoseTimer
+    lose_screen = %Lose
+    lose_audio = %LoseAudio
+    win_screen = %Win
+    win_audio = %WinAudio
+    background_music_player.stream = background_music
+    background_music_player.play()
+    lose_timer.timer.start(lose_timeout)
+    
+func on_win() -> void:
+    background_music_player.stop()
+    lose_timer.timer.stop()
+    win_screen.show()
+    win_audio.play()
+
+func on_lose() -> void:
+    background_music_player.stop()
+    lose_timer.timer.stop()
+    lose_screen.show()
+    lose_audio.play()
+
+func queue_lose_level() -> void:
+    lose_level.call_deferred()
+
+func lose_level() -> void:
+    TransitionGlobals.hand_angle = 0
+    TransitionGlobals.next_minigame = lose_scene
+    TransitionGlobals.context_text = "I guess I wasn't fast enough... I'll try again tomorrow."
+    get_tree().change_scene_to_packed(transition_scene)
+
+func queue_next_level() -> void:
+    next_level.call_deferred()
+
+func next_level() -> void:
+    TransitionGlobals.hand_angle = transition_hand_angle
+    TransitionGlobals.next_minigame = next_minigame
+    TransitionGlobals.context_text = transition_text
+    get_tree().change_scene_to_packed(transition_scene)
+
+func _on_lose_timer_timeout() -> void:
+    on_lose()
