2e1686a92001e15d67e974ef9b88c196ae723d16
not_luke <not_luke@fake.com>
2026-07-26 17:41:57 +1000

    beep

--- scenes/common/timer/level_timer.gd
+++ scenes/common/timer/level_timer.gd
@@ -17,47 +17,47 @@ var win_screen: Node2D
 var win_audio: AudioStreamPlayer
 
 func _ready() -> void:
-	background_music_player = %BackgroundMusic
-	lose_timer = %LoseTimer
-	lose_screen = %Lose
-	lose_audio = %LoseAudio
-	win_screen = %Win
-	win_audio = %WinAudio
-	if background_music:
-		background_music_player.stream = background_music
-		background_music_player.play()
-	lose_timer.timer.start(lose_timeout)
-	
+    background_music_player = %BackgroundMusic
+    lose_timer = %LoseTimer
+    lose_screen = %Lose
+    lose_audio = %LoseAudio
+    win_screen = %Win
+    win_audio = %WinAudio
+    if background_music:
+        background_music_player.stream = background_music
+        background_music_player.play()
+    lose_timer.timer.start(lose_timeout)
+    
 func on_win() -> void:
-	background_music_player.stop()
-	lose_timer.timer.stop()
-	win_screen.show()
-	win_audio.play()
+    background_music_player.stop()
+    lose_timer.timer.stop()
+    win_screen.show()
+    win_audio.play()
 
 func on_lose() -> void:
-	if background_music:
-		background_music_player.stop()
-	lose_timer.timer.stop()
-	lose_screen.show()
-	lose_audio.play()
+    if background_music:
+        background_music_player.stop()
+    lose_timer.timer.stop()
+    lose_screen.show()
+    lose_audio.play()
 
 func queue_lose_level() -> void:
-	lose_level.call_deferred()
+    lose_level.call_deferred()
 
 func lose_level() -> void:
-	TransitionGlobals.hand_angle = 0
-	TransitionGlobals.next_minigame = lose_scene
-	TransitionGlobals.context_text = "I guess I wasn't fast enough... I'll try again tomorrow."
-	get_tree().change_scene_to_packed(transition_scene)
+    TransitionGlobals.hand_angle = 0
+    TransitionGlobals.next_minigame = lose_scene
+    TransitionGlobals.context_text = "I guess I wasn't fast enough... I'll try again tomorrow."
+    get_tree().change_scene_to_packed(transition_scene)
 
 func queue_next_level() -> void:
-	next_level.call_deferred()
+    next_level.call_deferred()
 
 func next_level() -> void:
-	TransitionGlobals.hand_angle = transition_hand_angle
-	TransitionGlobals.next_minigame = next_minigame
-	TransitionGlobals.context_text = transition_text
-	get_tree().change_scene_to_packed(transition_scene)
+    TransitionGlobals.hand_angle = transition_hand_angle
+    TransitionGlobals.next_minigame = next_minigame
+    TransitionGlobals.context_text = transition_text
+    get_tree().change_scene_to_packed(transition_scene)
 
 func _on_lose_timer_timeout() -> void:
-	on_lose()
+    on_lose()
