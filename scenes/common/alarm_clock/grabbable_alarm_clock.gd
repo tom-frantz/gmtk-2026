e82f2222ca35571d0f642e52e7bd9ca901b39cce
Tom Frantz <thomas.albert.frantz@gmail.com>
2026-07-26 16:57:10 +1000

    feet: new books and alarm clock assets

--- scenes/common/alarm_clock/grabbable_alarm_clock.gd
+++ scenes/common/alarm_clock/grabbable_alarm_clock.gd
@@ -1,6 +1,9 @@
 extends Grabbable
 
-var was_pressed: bool = false
+
+func _on_alarm_button_body_entered(body: Node2D) -> void:
+	if collision_layer == 1:
+		emit_signal("my_fuckin_ace_signal", body)
 
 signal my_fuckin_ace_signal(body: Node2D)
 
