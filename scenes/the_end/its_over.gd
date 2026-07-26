1148f1cf2474808b4e7f6ede8e5c4b2cd2182d98
Matt Potts <mattiuspotts@gmail.com>
2026-07-26 17:52:46 +1000

    shitty credits

--- scenes/the_end/its_over.gd
+++ scenes/the_end/its_over.gd
@@ -3,14 +3,15 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    self.play("boom")
-    animation_finished.connect(_animation_finished)
+	self.play("boom")
+	animation_finished.connect(_animation_finished)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-    pass
-    
+	pass
+	
 func _animation_finished() -> void:
-    var the_end_label: Label = %game_over_label
-    the_end_label.visible = true
+	var the_end_label: Label = %game_over_label
+	the_end_label.visible = true
+	get_tree().change_scene_to_file("res://scenes/end_credits.tscn")
